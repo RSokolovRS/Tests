26497ef62b6d818a9015de9f2bcbff89d8b69a91
RSokolov <roman-27@inbox.ru>
2023-06-02 14:48:12 +0300

    New_Change

--- test_3.py
+++ test_3.py
@@ -14,18 +14,18 @@ queries = [
 
 def requests_distributed(_list):
     dict_ = {}
+    list_ = []
+    list_1 = []
     for item in _list:
         result = item.split()
+        dict_.update({len(result):result})
+        list_.append(dict_)
+    for my_dict in list_:
+        for key, value in my_dict.items():
+            percentage = round((len(value) / len(queries)) * 100, 2)
+            list_1.append(f'Поисковых запросов из {key} слова: {percentage}% ')
+        return list_1
 
-        if len(result) in dict_:
-            dict_[len(result)] += 1
-        
-        else:
-            dict_.update({len(result):1})
-    
-    for key, value in dict_.items():
-        percentage = round((value / len(queries)) * 100, 2)
-        return f'Поисковых запросов из {key} слова: {percentage}% ({value} запр.)'
 
 res = requests_distributed(queries)
 print(res)
